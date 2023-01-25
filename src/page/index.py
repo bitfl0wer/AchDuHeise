e2b1628361bb28a802f5f6ee2ec2a52643c9f853
bitfl0wer <florian@pro-weber.com>
2023-01-25 22:45:35 +0100

    update imports

--- src/page/index.py
+++ src/page/index.py
@@ -1,13 +1,13 @@
 from flask import Blueprint, jsonify
-from src.scraping.scraper import scrape_heise_article, Article
+from src.scraping.scraper import scrape_article, Article
 
 index = Blueprint("index", __name__)
 
 
 @index.route("/", methods=["GET"])
 def root():
-    article = scrape_heise_article(
-        "https://www.heise.de/hintergrund/Finanz-Analyse-Warum-Musks-Twitter-Kauf-so-ein-Desaster-ist-7469162.html"
+    article = scrape_article(
+        "https://www.heise.de/news/Cyber-Angriff-IT-der-TU-Freiberg-weitreichend-lahmgelegt-7469937.html?perbis"
     )
-    article_dict = article.dict()
-    return jsonify(article_dict), 200
+
+    return jsonify(article.dict()), 200
